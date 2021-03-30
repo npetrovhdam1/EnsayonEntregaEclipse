22cfdbb87a96150bb8be10d9c209975655d9b83b
eseminariosdam1 <eseminariosdam1@gmail.com>
2021-03-30 17:22:08 +0200

    Cambio de un colaborador - Saludo a Isabel

---
+++ src/prueba/modelo/PruebaGitEclipse.java
@@ -0,0 +1,13 @@
+package prueba.modelo;
+
+
+public class PruebaGitEclipse {
+
+	public static void main(String[] args) {
+		Saludo saludo = new Saludo();
+		System.out.println(saludo.saludar("Juan"));
+		 
+
+	}
+
+}
